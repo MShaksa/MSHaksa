3b2a50fb21892428a0191802358342405b27aecd
SamuelGalaxys <samuelthegalaxys@gmail.com>
2023-03-16 00:28:19 +0900

    zz

---
+++ data/time.js
@@ -0,0 +1,11 @@
+//냘짜 함수
+    var d = new Date();
+    var year = d.getFullYear().toString();
+    var month = (d.getMonth() + 1).toString();
+    var day = d.getDate().toString();
+    var hours = d.getHours().toString();
+    var min = d.getMinutes().toString();
+    var sec = d.getSeconds().toString();
+    var currentTime = year + "년 " + month + "월" + day+ "일  " + hours + ":" + min;
+    var dayTime = hours + ":" + min;
+    var monthTime = month + "월" + day+ "일  ";
