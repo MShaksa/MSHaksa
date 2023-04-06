c844d2080528192e0bdce7c003cff947588d8a2e
SamuelGalaxys <samuelthegalaxys@gmail.com>
2023-04-06 21:19:30 +0900

    ㅌㅌㅌ

--- modal.js
+++ modal.js
@@ -1,11 +1,11 @@
     //2초후 실행
     setTimeout(function() {
-        //	var element = document.getElementById("scrollz");
+        	var element = document.getElementById("modalUpdate-msg");
         //    element.scrollTop = element.scrollHeight;  //스크롤 내리기
 
 
 
 
-      //      $("#modalUpdateN").modal('show');
+            $("#modalUpdate-msg").modal('show');
     
         }, 1000);
